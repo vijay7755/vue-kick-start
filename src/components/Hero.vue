273c51c487e31199f8c715546c94b59b1401bd46
vijay7755 <vijaysekar2295@gmail.com>
2024-09-28 12:30:20 +0530

    computed and watch in depth

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,8 +1,7 @@
 <script setup>
-import { defineProps } from 'vue';
+import { defineProps, ref, computed, watch, onMounted } from 'vue';
 
-
-defineProps({
+const props = defineProps({
     title: {
         type: String,
         default: "Become a Vue dev"
@@ -10,8 +9,43 @@ defineProps({
     subTitle: {
         type: String,
         default: "All good company"
+    },
+    firstName: {
+        type: String
+    },
+    lastName: {
+        type: String
+    },
+})
+
+// Create local ref
+const localFirstName = ref(props.firstName)
+const localLastName = ref(props.lastName)
+
+// Watch to sync localref with props whenever props change
+watch(() => props.firstName, (newValue) => {
+    localFirstName.value = newValue
+})
+watch(() => props.lastName, (newValue) => {
+    localLastName.value = newValue
+})
+
+// Compute method to concadinate fullname
+const fullName = computed({
+    get() {
+        return `${localFirstName.value} ${localLastName.value}`
+    },
+    set(newValue) {
+        const [first, last] = newValue.split(' ')
+        localFirstName.value = first || ''
+        localLastName.value = last || ''
     }
 })
+
+onMounted(() => {
+    if (!props.firstName && !props.lastName) fullName.value = "Vijay Krishnan"
+})
+
 </script>
 
 <template>
@@ -24,6 +58,7 @@ defineProps({
                 <p class="my-4 text-xl text-white">
                     {{ subTitle }}
                 </p>
+                <h2 class="my-4 text-xl font-bold text-green-200">{{ fullName }}</h2>
             </div>
         </div>
     </section>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,11 +2,28 @@
 import Hero from "@/components/Hero.vue";
 import HomeCards from "@/components/HomeCards.vue";
 import JobListings from '@/components/JobListings.vue';
+import { ref } from "vue";
+
+const FN = ref("")
+const LN = ref("")
+
 </script>
 
 
 <template>
-    <Hero title="Vue Jobs at HighLevel" subTitle="A potential SaaS platform company" />
+    <div class="flex">
+        <form class="my-2">
+            <div class="my-3">
+                <label>First Name: </label>
+                <input class="border-2 border-gray-500 rounded" type="text" v-model="FN" />
+            </div>
+            <div>
+                <label>Last Name: </label>
+                <input class="border-2 border-gray-500 rounded" type="text" v-model="LN" />
+            </div>
+        </form>
+    </div>
+    <Hero title="Vue Jobs at HighLevel" subTitle="A potential SaaS platform company" :firstName="FN" :lastName="LN" />
     <HomeCards />
     <JobListings :limit="2" :showButton="true" />
 </template>
